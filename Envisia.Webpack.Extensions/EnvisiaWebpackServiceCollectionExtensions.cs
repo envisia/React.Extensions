c45f75ba31f1b21b3c4909a805e418bdbe37962d
Christian Schmitt <c.schmitt@envisia.de>
2022-02-02 14:26:36 +0100

    Revert "remove unnecessary react file version provider"
    This reverts commit 6b62e204776149d5048aaaf4ee44ce54295c8854.

--- Envisia.Webpack.Extensions/EnvisiaWebpackServiceCollectionExtensions.cs
+++ Envisia.Webpack.Extensions/EnvisiaWebpackServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Envisia.Webpack.Extensions.StaticFiles;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Envisia.Webpack.Extensions
@@ -21,6 +22,7 @@ namespace Envisia.Webpack.Extensions
         public static void AddHtmlStaticFileVersion(this IServiceCollection services)
         {
             services.AddMemoryCache();
+            services.AddSingleton<IFileVersionProvider, EnvisiaReactFileVersionProvider>();
 
             services.AddEvSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
