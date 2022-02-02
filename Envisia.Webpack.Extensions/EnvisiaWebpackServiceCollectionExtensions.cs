6b62e204776149d5048aaaf4ee44ce54295c8854
Christian Schmitt <c.schmitt@envisia.de>
2022-02-02 13:50:45 +0100

    remove unnecessary react file version provider

--- Envisia.Webpack.Extensions/EnvisiaWebpackServiceCollectionExtensions.cs
+++ Envisia.Webpack.Extensions/EnvisiaWebpackServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 using Envisia.Webpack.Extensions.StaticFiles;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Envisia.Webpack.Extensions
@@ -22,7 +21,6 @@ namespace Envisia.Webpack.Extensions
         public static void AddHtmlStaticFileVersion(this IServiceCollection services)
         {
             services.AddMemoryCache();
-            services.AddSingleton<IFileVersionProvider, EnvisiaReactFileVersionProvider>();
 
             services.AddEvSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
